d4f05fb7c7add08ceec1647d57089938ea88cbd3
Pratyush <pratyush@fusioncharts.com>
2017-03-04 17:52:04 +0530

    Added RootGroup tests

---
+++ test/test.rootgroup.js
@@ -0,0 +1,15 @@
+{
+  let RootGroup = Constructor.getComponent('RootGroup'),
+    BasicElement = Constructor.getComponent('BasicElement'),
+    bEl,
+    rootgroup = canvas.group
+  // Mocha tests
+  describe('RootGroup test', function(){
+    it('should be in canvas instance as root', function () {
+      expect(canvas.root).to.be.a(RootGroup)
+    })
+    it('should have draw method', function () {
+      expect(canvas.root.draw).to.be.a('function')
+    })
+  })
+}
